53887d7c496c8932b8b6f3e78ea53f3309f91b60
Ilya Veremeichyk <ya.inkogneto@outlook.com>
2017-11-15 23:35:06 +0300

    321

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,7 +9,20 @@ namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
+        private static IList<string> Messages = new List<string>();
+
         public ActionResult Index()
+        {
+            ViewBag.Messages = Messages;
+            return View();
+        }
+
+        public void Add(string message)
+        {
+            Messages.Add(message);
+        }
+
+        public ActionResult Private()
         {
             return View();
         }
